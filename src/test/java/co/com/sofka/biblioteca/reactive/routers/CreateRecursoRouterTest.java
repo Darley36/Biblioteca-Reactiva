6ac8c1252407a93ddc004ad56338985b30d37f06
Darley36 <darleybest@gmail.com>
2021-10-24 12:26:25 -0500

    create and update recurso router finished

--- src/test/java/co/com/sofka/biblioteca/reactive/routers/CreateRecursoRouterTest.java
+++ src/test/java/co/com/sofka/biblioteca/reactive/routers/CreateRecursoRouterTest.java
@@ -1,7 +1,67 @@
 package co.com.sofka.biblioteca.reactive.routers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.biblioteca.reactive.collections.Recurso;
+import co.com.sofka.biblioteca.reactive.model.RecursoDTO;
+import co.com.sofka.biblioteca.reactive.repositories.RepositorioRecurso;
+import co.com.sofka.biblioteca.reactive.usecases.CreateRecursoUseCase;
+import co.com.sofka.biblioteca.reactive.usecases.MapperUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {CreateRecursoRouter.class, CreateRecursoUseCase.class, MapperUtils.class})
 class CreateRecursoRouterTest {
+
+    @MockBean
+    private RepositorioRecurso repositorioRecurso;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+
+    @Test
+    void crearRecursoRouter() {
+
+        var recurso =new Recurso();
+        recurso.setId("xxx");
+        recurso.setName("las aventuras");
+        recurso.setAvailable(false);
+        recurso.setDate(LocalDate.parse("2020-10-19"));
+        recurso.setType("Libro");
+        recurso.setThematic("Terror");
+        recurso.setQuantityAvailable(2);
+        recurso.setQuantityBorrowed(2);
+
+
+        var recursoDTO = new RecursoDTO("xxx","las aventuras",true,
+                LocalDate.parse("2020-10-19"), "Libro",
+                "Terror",2,2 );
+
+        Mono<Recurso> mono = Mono.just(recurso);
+        when(repositorioRecurso.save(any())).thenReturn(mono);
+
+        webTestClient.post()
+                .uri("/crear")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(recursoDTO), RecursoDTO.class)
+                .exchange()
+                .expectStatus().isOk();
+    }
 
 }
